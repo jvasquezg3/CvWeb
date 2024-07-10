ca146f8432daa9ef96e5ef7b2b7e5e2a3668f928
jvasquezg3 <87217369+jvasquezg3@users.noreply.github.com>
2024-07-10 13:38:40 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+#from flask import Flask, render_template
+from flask import Flask, render_template, send_file
+import os  # Importar el módulo os para manejo de rutas y archivos
 
 
 app = Flask(__name__)
@@ -6,6 +8,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/download_cv')
+def download_cv():
+    filename = 'JvasquezCV.pdf'
+    directory = 'static/documents'
+    filepath = os.path.join(directory, filename)
+    if os.path.isfile(filepath):
+        return send_file(filepath, as_attachment=True)
+    else:
+        return "Archivo no encontrado", 404
     
 
 if __name__ == '__main__':
